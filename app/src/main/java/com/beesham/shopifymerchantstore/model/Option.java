f128509471f32b0d0a66df744a4142a8b30e598a
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-12-27 18:17:38 -0500

    Add POJO's under model

---
+++ app/src/main/java/com/beesham/shopifymerchantstore/model/Option.java
@@ -0,0 +1,66 @@
+
+package com.beesham.shopifymerchantstore.model;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Option {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("product_id")
+    @Expose
+    private int productId;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("position")
+    @Expose
+    private int position;
+    @SerializedName("values")
+    @Expose
+    private List<String> values = null;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public List<String> getValues() {
+        return values;
+    }
+
+    public void setValues(List<String> values) {
+        this.values = values;
+    }
+
+}
