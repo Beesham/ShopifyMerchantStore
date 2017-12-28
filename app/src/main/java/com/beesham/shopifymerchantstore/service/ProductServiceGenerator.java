085493d45e20b45e8c16802d202131ac8892d23e
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-12-27 23:24:04 -0500

    Implement ProductServiceGenerator

---
+++ app/src/main/java/com/beesham/shopifymerchantstore/service/ProductServiceGenerator.java
@@ -0,0 +1,39 @@
+package com.beesham.shopifymerchantstore.service;
+
+import android.text.TextUtils;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Beesham on 12/27/2017.
+ */
+
+public class ProductServiceGenerator {
+
+    public static final String SHOPIFY_BASE_URL = "https://shopicruit.myshopify.com/admin/";
+
+    private static OkHttpClient client = new OkHttpClient().newBuilder().build();
+
+    private static Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
+            .baseUrl(SHOPIFY_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = retrofitBuilder.build();
+
+    public static <S> S createService(Class<S> serviceClass, final String authToken) {
+        if(!TextUtils.isEmpty(authToken)) {
+            AuthenticationInterceptor interceptor = new AuthenticationInterceptor(authToken);
+
+            if(!client.interceptors().contains(interceptor)){
+                client = client.newBuilder().addInterceptor(interceptor).build();
+                retrofitBuilder = retrofit.newBuilder();
+                retrofit = retrofitBuilder.client(client).build();
+            }
+        }
+
+        return retrofit.create(serviceClass);
+    }
+
+}
