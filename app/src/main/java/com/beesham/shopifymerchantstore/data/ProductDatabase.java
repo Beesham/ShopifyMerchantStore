0ee7653b46f16d0f224f4c034291968b80f8f5a6
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-12-28 21:13:47 -0500

    Implement ProductDatabase.class

--- app/src/main/java/com/beesham/shopifymerchantstore/data/ProductDatabase.java
+++ app/src/main/java/com/beesham/shopifymerchantstore/data/ProductDatabase.java
@@ -1,8 +1,18 @@
 package com.beesham.shopifymerchantstore.data;
 
+import net.simonvt.schematic.annotation.Database;
+import net.simonvt.schematic.annotation.Table;
+
 /**
  * Created by Beesham on 12/27/2017.
  */
 
+@Database(version = ProductDatabase.VERSION)
 public class ProductDatabase {
+    public static final int VERSION = 1;
+
+    @Table(Columns.ProductColumns.class) public static final String PRODUCT = "product";
+    @Table(Columns.VariantColumns.class) public static final String VARIANT = "variant";
+    @Table(Columns.OptionColumns.class) public static final String OPTION = "option";
+
 }
