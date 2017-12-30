764caa73bb71d89d0c8f627eee3dcbaf102bf6a0
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-12-30 11:52:22 -0500

    Add RecyclerView to ProdcutFragment

---
+++ app/src/main/java/com/beesham/shopifymerchantstore/adapters/ProductsRecyclerViewAdapter.java
@@ -0,0 +1,25 @@
+package com.beesham.shopifymerchantstore.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Beesham on 12/30/2017.
+ */
+
+public class ProductsRecyclerViewAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/beesham/shopifymerchantstore/ui/ProductFragment.java
+++ app/src/main/java/com/beesham/shopifymerchantstore/ui/ProductFragment.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.beesham.shopifymerchantstore.R;
+import com.beesham.shopifymerchantstore.adapters.ProductsRecyclerViewAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,53 +22,41 @@ import com.beesham.shopifymerchantstore.R;
  * create an instance of this fragment.
  */
 public class ProductFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private RecyclerView mRecyclerView;
+    private ProductsRecyclerViewAdapter mAdapter;
 
     public ProductFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProductFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProductFragment newInstance(String param1, String param2) {
+    public static ProductFragment newInstance() {
         ProductFragment fragment = new ProductFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+/*        Bundle args = new Bundle();
+        fragment.setArguments(args);*/
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product, container, false);
+        final View view = inflater.inflate(R.layout.fragment_product, container, false);
+
+        mRecyclerView = view.findViewById(R.id.products_recycler_view);
+        mAdapter = new ProductsRecyclerViewAdapter();
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
